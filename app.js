a264893a35677345886c2b02f5cbe17f9bb08e9c
etandmouse <etandmouse@gmail.copm>
2023-03-30 21:30:12 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+const epxhps = require('express-handlebars')
+
 const users = require("./users.json")
 const port = 3000
 
+app.engine('hbs', epxhps({ defaultlayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.send("This is simple login project")
+  res.render('index')
 })
 
 app.listen(port, () => {
